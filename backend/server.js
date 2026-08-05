f24b123d06df34f76f4e1053370167539522ed46
VipulPhatanagare <vipulphatangare3@gmail.com>
2026-08-06 00:22:28 +0530

    Lower default worker concurrency to 3 to optimize VPS CPU and RAM usage

--- backend/server.js
+++ backend/server.js
@@ -95,8 +95,8 @@ async function launchScraperProcess({ jobAge = 2, startPage = 1, pages = 150, tr
     '--pages', pages.toString(),
     '--job-age', jobAge.toString(),
     '--batch-size', '10',
-    '--srp-workers', '5',
-    '--detail-workers', '5'
+    '--srp-workers', '3',
+    '--detail-workers', '3'
   ];
 
   console.log(`[Scraper Engine] Launching python ${args.join(' ')}`);
